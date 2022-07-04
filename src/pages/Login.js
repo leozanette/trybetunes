a37d29185af98660cbd8727c6ea63285542d713d
Leonardo Zanette <leonardobzanette@gmail.com>
2022-07-04 12:18:45 -0300

    Requisito 2 - DONE

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,67 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
-  render() {
-    return (
+  constructor() {
+    super();
+    this.state = {
+      login: '',
+      loading: true,
+      redirect: false,
+    };
+  }
 
+  onInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    // console.log(event.target.value);
+  }
+
+  clickLogin = (event) => {
+    const { login } = this.state;
+    event.preventDefault();
+    this.setState({ loading: false },
+      async () => {
+        await createUser({ name: login });
+        this.setState({
+          loading: true,
+          redirect: true });
+      });
+  }
+
+  render() {
+    const { login, loading, redirect } = this.state;
+    const minLength = 3;
+    if (redirect) return (<Redirect to="/search" />);
+    return (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        { loading
+          ? (
+            <form>
+              <label htmlFor="login">
+                Nome
+                <input
+                  type="text"
+                  name="login"
+                  data-testid="login-name-input"
+                  value={ login }
+                  onChange={ this.onInputChange }
+                />
+              </label>
+              <button
+                disabled={ login.length < minLength }
+                type="submit"
+                data-testid="login-submit-button"
+                onClick={ this.clickLogin }
+              >
+                Entrar
+
+              </button>
+            </form>
+          )
+          : <Loading />}
       </div>
     );
   }
