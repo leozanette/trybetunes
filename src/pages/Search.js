2931e945350cbea13cddb8479b1b19644e8941f0
Leonardo Zanette <leonardobzanette@gmail.com>
2022-07-05 09:38:50 -0300

    Requisito 5 - DONE

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,12 +3,39 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
-  render() {
-    return (
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
 
+  onInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { search } = this.state;
+    const minLength = 2;
+    return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <input
+          placeholder="Nome do Artista"
+          data-testid="search-artist-input"
+          type="text"
+          name="search"
+          value={ search }
+          onChange={ this.onInputChange }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ search.length < minLength }
+        >
+          Procurar
+
+        </button>
       </div>
     );
   }
